d1445b34a070131dc77f598a816ff2420b23a698
AKAD0 <onixon2010@gmail.com>
2023-04-15 05:37:17 +0300

    mysite/mysite/__pycache__/__init__.cpython-311.pyc 	mysite/mysite/__pycache__/settings.cpython-311.pyc 	mysite/mysite/__pycache__/urls.cpython-311.pyc 	mysite/mysite/__pycache__/wsgi.cpython-311.pyc 	mysite/polls/__pycache__/__init__.cpython-311.pyc 	mysite/polls/__pycache__/admin.cpython-311.pyc 	mysite/polls/__pycache__/apps.cpython-311.pyc 	mysite/polls/__pycache__/models.cpython-311.pyc 	mysite/polls/__pycache__/urls.cpython-311.pyc 	mysite/polls/__pycache__/views.cpython-311.pyc 	mysite/polls/migrations/__pycache__/0001_initial.cpython-311.pyc 	mysite/polls/migrations/__pycache__/0002_choice_choise_img.cpython-311.pyc 	mysite/polls/migrations/__pycache__/0003_remove_choice_choise_img_question_question_img.cpython-311.pyc 	mysite/polls/migrations/__pycache__/__init__.cpython-311.pyc 	mysite/static/

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x&f&m54x=xl=c2v#kyog_^#_ieh3zq^*-4jl2&bu3%c1lp+vq_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '217.10.41.168', '127.0.0.2']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
